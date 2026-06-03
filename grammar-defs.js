81ccb667ceff699dd17b487972a1cd06e5d9df05
Ivo Julca <ivojulca@hotmail.com>
2026-06-03 14:06:07 -0500

    Ban exitwhen outside loops

---
+++ grammar-defs.js
@@ -0,0 +1,559 @@
+"use strict";
+
+/**
+ * @file Parser for the JASS2 language
+ * @author Leonardo Julca <ivojulca@gmail.com>
+ * @license MIT
+ */
+
+/// <reference types="tree-sitter-cli/dsl" />
+// @ts-check
+
+const PRECEDENCES = {
+    'and': 1,
+	'or': 2,
+	'==': 10,
+	'!=': 10,
+	'<': 15,
+	'>': 15,
+	'<=': 15,
+	'>=': 15,
+	'+': 20,
+	'-': 20,
+	'*': 21,
+	'/': 21,
+	'not': 30,
+	'neg': 30,
+	'pos': 30,
+}
+
+const binaryOperators = [
+	'and', 'or', '==', '!=', '<', '>', '<=', '>=', '+', '-', '*', '/',
+];
+
+function repeat1WithDelimiter(nodeTypes, arg, delim) {
+	return seq(nodeTypes[arg], repeat(seq(delim, nodeTypes[arg])));
+}
+
+function makeBinaryExpressionRule(operator) {
+	return prec.left(PRECEDENCES[operator], seq(
+		field('lhs', this.Expression),
+		field('operator', alias(operator, this.Operator)),
+		field('rhs', this.Expression),
+	));
+}
+
+const helpers = {
+	loop: false,
+	rules: {
+		AnyStatementsOrNewLines(nodeTypes) {
+			return repeat(choice(
+				seq(nodeTypes.RStatement, nodeTypes.NewLine),
+				nodeTypes.NewLine,
+			));
+		},
+	},
+	setForkWhetherInLoop(baseName, def) {
+		const outsideName = `R${baseName}`;
+		const insideName = `L${baseName}`;
+		const outsideDef = def.bind(helpers);
+		const insideDef = def.bind(helpersLoopCtx);
+		return {
+			[outsideName](nodeTypes) {
+				return outsideDef(nodeTypes);
+			},
+			[insideName](nodeTypes) {
+				return insideDef(nodeTypes);
+			},
+		};
+	},
+	getForkWhetherInLoop(nodeTypes, baseName) {
+		return nodeTypes[`R${baseName}`];
+	},
+	listForkedNodes(nodeTypes, baseName) {
+		return [nodeTypes[`R${baseName}`], nodeTypes[`L${baseName}`]];
+	},
+};
+
+const helpersLoopCtx = {
+	loop: true,
+	rules: {
+		AnyStatementsOrNewLines(nodeTypes) {
+			return repeat(choice(
+				seq(nodeTypes.LStatement, nodeTypes.NewLine),
+				nodeTypes.NewLine,
+			));
+		},
+	},
+	getForkWhetherInLoop(nodeTypes, baseName) {
+		return nodeTypes[`L${baseName}`];
+	},
+};
+
+const rules = {
+	NewLine() {
+		return /\r|\r?\n/;
+	},
+
+	Identifier() {
+		return /[a-zA-Z]([a-zA-Z0-9_]*)/;
+	},
+
+	AtomicType(nodeTypes) {
+		return nodeTypes.Identifier;
+	},
+
+	ArrayType(nodeTypes) {
+		return seq(nodeTypes.Identifier, 'array');
+	},
+
+	Type(nodeTypes) {
+		return choice(nodeTypes.AtomicType, nodeTypes.ArrayType);
+	},
+
+	ArrayElement(nodeTypes) {
+		return seq(
+			field('array', nodeTypes.Identifier),
+			'[',
+			field('index', nodeTypes.Expression),
+			']',
+		);
+	},
+
+	Comment(nodeTypes) {
+		// Rust dot includes every entity, except LF.
+		return token(seq('//', /.*/));
+	},
+
+	String(nodeTypes) {
+		return token(seq(
+			'"',
+			repeat(choice(
+				/[^"\\]/,
+				/\\./,
+			)),
+			'"',
+		));
+	},
+
+	DecimalInteger(nodeTypes) {
+		return token(seq(
+			optional('-'),
+			choice(
+				'0',
+				/[1-9]([0-9]*)/,
+			),
+		));
+	},
+
+	OctalInteger(nodeTypes) {
+		return /-?0([0-7]+)/;
+	},
+
+	HexInteger(nodeTypes) {
+		return token(seq(
+			optional('-'),
+			choice('0x', '0X', '$'),
+			/[0-9a-fA-F]+/
+		));
+	},
+
+	Integer(nodeTypes) {
+		return choice(
+			nodeTypes.DecimalInteger,
+			nodeTypes.OctalInteger,
+			nodeTypes.HexInteger,
+		);
+	},
+
+	Real(nodeTypes) {
+		return token(
+			seq(
+				optional('-'),
+				choice(
+					/\.[0-9]+/,
+					/[0-9]+\.[0-9]*/,
+				),
+			),
+		);
+	},
+
+	Byte(nodeTypes) {
+		return token(seq(
+			"'",
+			choice(
+				/[^'\\]/,
+				'\\(\\|n|r|t|f|b)',
+			),
+			"'",
+		));
+	},
+
+	FourCC(nodeTypes) {
+		return token(seq(
+			"'",
+			/[^']/,
+			/[^']/,
+			/[^']/,
+			/[^']/,
+			"'",
+		));
+	},
+
+	Literal(nodeTypes) {
+		return choice(
+			nodeTypes.String,
+			nodeTypes.Integer,
+			nodeTypes.Real,
+			nodeTypes.FourCC,
+			nodeTypes.Byte,
+		);
+	},
+
+	TopLevel(nodeTypes) {
+		return choice(
+			nodeTypes.TypeDeclaration,
+			nodeTypes.NativeDeclaration,
+			nodeTypes.FunctionDeclaration,
+			nodeTypes.GlobalsBlock,
+		);
+	},
+
+	TypeDeclaration(nodeTypes) {
+		return seq(
+			'type',
+			field('name', nodeTypes.Identifier),
+			'extends',
+			field('super', nodeTypes.Identifier),
+		);
+	},
+
+	GlobalsBlock(nodeTypes) {
+		return seq(
+			'globals',
+			repeat(choice(
+				seq(nodeTypes.GlobalDeclarationStatement, nodeTypes.NewLine),
+				nodeTypes.NewLine,
+			)),
+			'endglobals',
+		);
+	},
+
+	GlobalDeclarationStatement(nodeTypes) {
+		return choice(
+			seq(
+				optional(nodeTypes.DeclareAttributes),
+				field('type', nodeTypes.AtomicType),
+				field('name', nodeTypes.Identifier),
+				optional(nodeTypes.Initializer),
+			),
+			seq(
+				field('type', nodeTypes.ArrayType),
+				field('name', nodeTypes.Identifier),
+			),
+		);
+	},
+
+	FunctionSignature(nodeTypes) {
+		return seq(
+			field('name', nodeTypes.Identifier),
+			'takes',
+			field('input', choice(
+				alias('nothing', nodeTypes.Empty),
+				nodeTypes.FunctionParameterList,
+			)),
+			'returns',
+			field('output', choice(
+				alias('nothing', nodeTypes.None),
+				nodeTypes.Identifier,
+			))
+		);
+	},
+
+	FunctionParameterList(nodeTypes) {
+		return repeat1WithDelimiter(nodeTypes, 'FunctionParameter', ',');
+	},
+
+	FunctionParameter(nodeTypes) {
+		return seq(
+			field('type', nodeTypes.Identifier),
+			field('name', nodeTypes.Identifier),
+		);
+	},
+
+	NativeDeclaration(nodeTypes) {
+		return seq(
+			optional(nodeTypes.NativeAttributes),
+			'native',
+			nodeTypes.FunctionSignature,
+		);
+	},
+
+	NativeAttributes(nodeTypes) {
+		return 'constant';
+	},
+
+	FunctionDeclaration(nodeTypes) {
+		return seq(
+			optional(nodeTypes.FunctionAttributes),
+			'function',
+			nodeTypes.FunctionSignature,
+			nodeTypes.FunctionBody,
+			'endfunction',
+		);
+	},
+
+	FunctionAttributes(nodeTypes) {
+		return 'constant';
+	},
+
+	FunctionBody(nodeTypes) {
+		return seq(
+			nodeTypes.NewLine,
+			repeat(choice(
+				seq(nodeTypes.LocalDeclarationStatement, nodeTypes.NewLine),
+				nodeTypes.NewLine,
+			)),
+			helpers.rules.AnyStatementsOrNewLines(nodeTypes),
+		);
+	},
+
+	LocalDeclarationStatement(nodeTypes) {
+		return choice(
+			seq(
+				'local',
+				optional(nodeTypes.DeclareAttributes),
+				field('type', nodeTypes.AtomicType),
+				field('name', nodeTypes.Identifier),
+				optional(nodeTypes.Initializer),
+			),
+			seq(
+				'local',
+				field('type', nodeTypes.ArrayType),
+				field('name', nodeTypes.Identifier),
+			),
+		);
+	},
+
+	DeclareAttributes(nodeTypes) {
+		return 'constant';
+	},
+
+	Initializer(nodeTypes) {
+		return seq('=', field('value', nodeTypes.Expression));
+	},
+
+	...helpers.setForkWhetherInLoop('Statement', function (nodeTypes) {
+		return choice(
+			this.getForkWhetherInLoop(nodeTypes, 'IfStatement'),
+			nodeTypes.LoopStatement,
+			nodeTypes.ReturnStatement,
+			nodeTypes.SetStatement,
+			nodeTypes.CallStatement,
+			...(this.loop ? [nodeTypes.ExitWhenStatement] : []),
+		);
+	}),
+
+	...helpers.setForkWhetherInLoop('IfStatement', function (nodeTypes) {
+		return seq(
+			'if',
+			field('test', nodeTypes.Expression),
+			'then',
+			this.getForkWhetherInLoop(nodeTypes, 'Consequent'),
+			repeat(this.getForkWhetherInLoop(nodeTypes, 'ElseIfStatement')),
+			optional(this.getForkWhetherInLoop(nodeTypes, 'ElseStatement')),
+			'endif',
+		);
+	}),
+
+	...helpers.setForkWhetherInLoop('ElseIfStatement', function (nodeTypes) {
+		return seq(
+			'elseif',
+			field('test', nodeTypes.Expression),
+			'then',
+			this.getForkWhetherInLoop(nodeTypes, 'Consequent')
+		);
+	}),
+
+	...helpers.setForkWhetherInLoop('ElseStatement', function (nodeTypes) {
+		return seq(
+			'else',
+			this.getForkWhetherInLoop(nodeTypes, 'Alternate')
+		);
+	}),
+
+	...helpers.setForkWhetherInLoop('Consequent', function (nodeTypes) {
+		return seq(
+			nodeTypes.NewLine,
+			this.rules.AnyStatementsOrNewLines(nodeTypes),
+		);
+	}),
+
+	...helpers.setForkWhetherInLoop('Alternate', function (nodeTypes) {
+		return seq(
+			nodeTypes.NewLine,
+			this.rules.AnyStatementsOrNewLines(nodeTypes),
+		);
+	}),
+
+	LoopStatement(nodeTypes) {
+		return seq(
+			'loop',
+			nodeTypes.NewLine,
+			helpersLoopCtx.rules.AnyStatementsOrNewLines(nodeTypes),
+			'endloop',
+		);
+	},
+
+	ExitWhenStatement(nodeTypes) {
+		return seq(
+			'exitwhen',
+			nodeTypes.Expression,
+		);
+	},
+
+	ReturnStatement(nodeTypes) {
+		return seq(
+			'return',
+			optional(nodeTypes.Expression),
+		);
+	},
+
+	SetStatement(nodeTypes) {
+		return seq(
+			'set',
+			field('binding', nodeTypes.Binding),
+			nodeTypes.Initializer,
+		);
+	},
+
+	Binding(nodeTypes) {
+		return choice(
+			nodeTypes.Identifier,
+			nodeTypes.ArrayElement,
+		);
+	},
+
+	CallStatement(nodeTypes) {
+		return seq(
+			'call',
+			nodeTypes.CallExpression,
+		);
+	},
+
+	CallExpression(nodeTypes) {
+		return seq(
+			field('callee', nodeTypes.Identifier),
+			'(',
+			optional(field('arguments', repeat1WithDelimiter(nodeTypes, 'FunctionArgument', ','))),
+			')',
+		);
+	},
+
+	Expression(nodeTypes) {
+		return choice(
+			nodeTypes.PrimaryExpression,
+			nodeTypes.UnaryExpression,
+			nodeTypes.BinaryExpression,
+		);
+	},
+
+	PrimaryExpression(nodeTypes) {
+		return choice(
+			nodeTypes.Literal,
+			nodeTypes.Identifier,
+			nodeTypes.ArrayElement,
+			nodeTypes.CallExpression,
+			nodeTypes.ParenthesizedExpression,
+		);
+	},
+
+	UnaryExpression(nodeTypes) {
+		return choice(
+			nodeTypes.NotExpression,
+			nodeTypes.NegativeExpression,
+			nodeTypes.PositiveExpression,
+		);
+	},
+
+	NotExpression(nodeTypes) {
+		return prec.right(PRECEDENCES.not, seq('not', nodeTypes.Expression));
+	},
+
+	NegativeExpression(nodeTypes) {
+		return prec.right(PRECEDENCES.neg, seq('-', nodeTypes.Expression));
+	},
+
+	PositiveExpression(nodeTypes) {
+		return prec.right(PRECEDENCES.pos, seq('+', nodeTypes.Expression));
+	},
+
+	BinaryExpression(nodeTypes) {
+		return choice(...binaryOperators.map(makeBinaryExpressionRule, nodeTypes));
+	},
+
+	ParenthesizedExpression(nodeTypes) {
+		return seq('(', nodeTypes.Expression, ')');
+	},
+
+	FunctionArgument(nodeTypes) {
+		return choice(
+			nodeTypes.Expression,
+			nodeTypes.CodeReference,
+		);
+	},
+
+	CodeReference(nodeTypes) {
+		return seq('function', nodeTypes.Identifier);
+	},
+};
+
+const root = function program(nodeTypes) {
+	return seq(
+		repeat(choice(
+			seq(nodeTypes.TopLevel, nodeTypes.NewLine),
+			nodeTypes.NewLine,
+		)),
+		optional(nodeTypes.TopLevel),
+	);
+};
+
+module.exports = {
+	root,
+
+	baseDefinitions: {
+		name: 'jass',
+
+		extras(nodeTypes) {
+			return [
+				/ |\t/,
+				nodeTypes.Comment,
+			];
+		},
+
+		word(nodeTypes) {
+			return nodeTypes.Identifier;
+		},
+
+		inline(nodeTypes) {
+			return [
+				nodeTypes.TopLevel,
+				...helpers.listForkedNodes(nodeTypes, 'Statement'),
+				nodeTypes.Binding,
+				nodeTypes.Expression,
+				nodeTypes.PrimaryExpression,
+				nodeTypes.UnaryExpression,
+				//nodeTypes.BinaryExpression,
+				nodeTypes.Integer,
+			];
+		},
+
+		conflicts(nodeTypes) {
+			return [
+				[nodeTypes.FunctionBody],
+			];
+		},
+
+		rules,
+	},	
+};
